7a543f8fe90c3075e36b920f0719d34d8388ddb3
Nitin kumar <nk999549@gmail.com>
2025-05-18 01:18:13 +0530

    fix question manually

--- app/admin/question/page.tsx
+++ app/admin/question/page.tsx
@@ -6,30 +6,41 @@ import UploadQuestionsFromWord from './Bulkquestion';
 import New from './New';
 
 const QuestionPage = () => {
-  const [showUpload, setShowUpload] = useState(false);
+const [viewMode, setViewMode] = useState<'manual' | 'excel' | 'rounds'>('manual');
+
 
   return (
-    <div className="p-5 h-screen overflow-y-auto bg-gray-50 w-full">
-      <div className="flex justify-end items-end mb-4">
-        {/* <h1 className="text-xl font-semibold">Question Management</h1> */}
-        <div className="space-x-2">
-          <button
-            onClick={() => setShowUpload(false)}
-            className={`px-4 py-2 rounded ${!showUpload ? 'bg-blue-600 text-white' : 'bg-white border'}`}
-          >
-            Question Manager
-          </button>
-          <button
-            onClick={() => setShowUpload(true)}
-            className={`px-4 py-2 rounded ${showUpload ? 'bg-blue-600 text-white' : 'bg-white border'}`}
-          >
-            Upload Excel File
-          </button>
-        </div>
-      </div>
+    <div className='p-5 h-screen overflow-y-auto bg-gray-50 w-full'> 
+  <div className=" flex justify-end gap-3">
+      <button
+    onClick={() => setViewMode('rounds')}
+    className={`px-4 py-2 rounded ${viewMode === 'rounds' ? 'bg-blue-600 text-white' : 'bg-white border'}`}
+  >
+   By Rounds
+  </button>
+  <button
+    onClick={() => setViewMode('manual')}
+    className={`px-4 py-2 rounded ${viewMode === 'manual' ? 'bg-blue-600 text-white' : 'bg-white border'}`}
+  >
+    Upload Manually
+  </button>
+  <button
+    onClick={() => setViewMode('excel')}
+    className={`px-4 py-2 rounded ${viewMode === 'excel' ? 'bg-blue-600 text-white' : 'bg-white border'}`}
+  >
+    Upload Excel File
+  </button>
 
-      {showUpload ? <UploadQuestionsFromWord /> : <New />}
-    </div>
+</div>
+<div>
+
+
+  {viewMode === 'rounds' && <New />}
+  {viewMode === 'manual' && <QuestionManager />}
+  {viewMode === 'excel' && <UploadQuestionsFromWord />}
+  </div>
+  </div>
+
   );
 };
 
